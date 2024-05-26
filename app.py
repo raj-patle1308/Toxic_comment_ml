91e0d5f93f83d075e69ae7911f44e4d4d928cc86
Raj Patle <136676766+raj-patle1308@users.noreply.github.com>
2024-05-26 17:24:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import subprocess
-
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import roc_auc_score, roc_curve
+import pickle
+import nltk
+import re
 def run_toxic():
     subprocess.run(["python", "toxic.py"])
 
