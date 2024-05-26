5d97f857ae5653f251f3a1b6aa161a1356562a3b
Raj Patle <136676766+raj-patle1308@users.noreply.github.com>
2024-05-26 17:27:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def run_toxic():
     import pickle
     import nltk
     import re
-    from tkinter import *
+    import tkinter 
     from tkinter import messagebox
     
     # Download NLTK resources
